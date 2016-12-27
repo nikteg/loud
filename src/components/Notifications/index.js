c84d151bcf5bc7020299e5e04a278e74c4f13f04
Niklas <me@sodapop.se>
2016-12-27 16:31:23 +0100

    Fix not being able to render 'undefined'

--- src/components/Notifications/index.js
+++ src/components/Notifications/index.js
@@ -4,7 +4,7 @@ import { connect } from "react-redux";
 import "./style.styl";
 
 const Notifications = connect(state => ({
-  message: state.Notification.messages[0],
+  message: state.Notification.messages[0] || null,
 }))(props => (
   props.message && <div className="Notifications">
     {props.message}
